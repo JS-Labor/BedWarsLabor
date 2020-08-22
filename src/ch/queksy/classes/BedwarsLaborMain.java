5b9162eaa450d560059dd1b9cf611ffbdd8d8af0
ichbins169 <60796942+ichbins169@users.noreply.github.com>
2020-08-22 22:07:48 +0200

    misc

---
+++ src/ch/queksy/classes/BedwarsLaborMain.java
@@ -0,0 +1,20 @@
+//Class by Joel Schaller made at 18:53 on 22.08.2020
+
+package ch.queksy.classes;
+
+import ch.queksy.gadgets.TNTBallGadget;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class BedwarsLaborMain {
+
+    public void onEnable() {
+
+
+    }
+
+    public void onDisable() {
+
+
+    }
+}
