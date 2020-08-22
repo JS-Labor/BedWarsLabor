9bc562e1568a0d18ebd5e121647c0c421f90c229
ichbins169 <60796942+ichbins169@users.noreply.github.com>
2020-08-22 22:07:40 +0200

    create TNTBallGadget

---
+++ src/ch/queksy/gadgets/TNTBallGadget.java
@@ -0,0 +1,36 @@
+//Class by Joel Schaller made at 21:40 on 22.08.2020
+
+package ch.queksy.gadgets;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+
+public class TNTBallGadget extends ItemStack {
+
+    private ItemMeta fireballMeta = null;
+    ArrayList<String> fireballLore = new ArrayList<String>();
+
+    public TNTBallGadget() {
+
+        super(Material.FIREBALL, 1);
+        fireballMeta = getItemMeta();
+        create();
+    }
+
+    public void create() {
+
+        fireballMeta.setDisplayName(ChatColor.GOLD + "TNTBall");
+
+        fireballLore.add(ChatColor.GREEN + "Use this Ball to shoot like a Ghast!");
+        fireballMeta.setLore(fireballLore);
+
+        setItemMeta(fireballMeta);
+    }
+
+
+}
