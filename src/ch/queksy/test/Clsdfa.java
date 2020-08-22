f8bae59fd9e27a8661e338e2a6dc11c15939939f
ichbins169 <60796942+ichbins169@users.noreply.github.com>
2020-08-22 18:36:35 +0200

    testcommit

---
+++ src/ch/queksy/test/Clsdfa.java
@@ -0,0 +1,12 @@
+//Class by Joel Schaller made at 18:33 on 22.08.2020
+
+package ch.queksy.test;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Clsdfa {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
