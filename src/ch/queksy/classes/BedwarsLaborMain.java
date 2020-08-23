1e7647ee5a88bfc41320c5fdc2b0c67fd1ce1d5c
ichbins169 <60796942+ichbins169@users.noreply.github.com>
2020-08-23 11:22:08 +0200

    finished ItemBuilder.java

--- src/ch/queksy/classes/BedwarsLaborMain.java
+++ src/ch/queksy/classes/BedwarsLaborMain.java
@@ -2,6 +2,7 @@
 
 package ch.queksy.classes;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BedwarsLaborMain extends JavaPlugin {
--- src/ch/queksy/utils/itemBuilder/ItemBuilder.java
+++ src/ch/queksy/utils/itemBuilder/ItemBuilder.java
@@ -3,8 +3,12 @@
 package ch.queksy.utils.itemBuilder;
 
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
 
 public class ItemBuilder {
 
@@ -16,10 +20,51 @@ public class ItemBuilder {
         itemMeta = item.getItemMeta();
     }
 
-    public ItemBuilder setName(String name) {
+    public ItemBuilder setAmount(int amount) {
+        item.setAmount(amount);
+        return this;
+    }
+
+    public ItemBuilder setType(Material material) {
+        item.setType(material);
+        return this;
+    }
+
+    public ItemBuilder setDurability(short durability) {
+        item.setDurability(durability);
+        return this;
+    }
+
+    public ItemBuilder addUnsafeEnchantement(Enchantment enchantment, int level) {
+        item.addUnsafeEnchantment(enchantment, level);
+        return this;
+    }
+
+    public ItemBuilder addEnchantement(Enchantment enchantment, int level) {
+        item.addEnchantment(enchantment, level);
+        return this;
+    }
+
+    public ItemBuilder setDisplayName(String name) {
         itemMeta.setDisplayName(name);
         return this;
     }
+
+    public ItemBuilder setLore(String... lore) {
+        itemMeta.setLore(Arrays.asList(lore));
+        return this;
+    }
+
+    public ItemBuilder addEnchant(Enchantment enchantment, int i, boolean b) {
+        itemMeta.addEnchant(enchantment, i, b);
+        return this;
+    }
+
+    public ItemBuilder addItemFlags(ItemFlag... itemFlags) {
+        itemMeta.addItemFlags(itemFlags);
+        return this;
+    }
+
 
     public ItemStack build() {
         item.setItemMeta(itemMeta);
